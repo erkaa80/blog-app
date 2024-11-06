ba4a449ad389f6470410d497aff327452307329c
Erdenetsetseg <erica@Erdenetsetsegs-MacBook-Pro.local>
2024-11-06 11:34:40 -0600

    changed commit

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -3,14 +3,15 @@ import { Box, Modal as MuiModal } from "@mui/material";
 
 const style = {
   position: "absolute",
-  top: "25%",
-  left: "40%",
-  width: "400px",
-  height: "300px",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50% )",
+  width: "400",
   borderRadius: "8px",
   bgcolor: "background.paper",
   p: 4,
   outline: "none",
+  fontSize: "14",
 };
 
 export const Modal = (props) => {
