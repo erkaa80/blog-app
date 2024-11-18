40a3100ae7233ff1c0e3fc5b7e2e5315cfe85270
Erdenetsetseg <erica@Erdenetsetsegs-MacBook-Pro.local>
2024-11-18 11:04:22 -0600

    added files

--- src/pages/blogs/BlogsPage.js
+++ src/pages/blogs/BlogsPage.js
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
-import { Button, CreateBlogModal, Header } from "../../components";
+import {
+  Button,
+  CreateBlogModal,
+  CreateTagModal,
+  Header,
+} from "../../components";
 import "./BlogsPage.css";
+import { useBlogContext } from "../../context";
 
 export const BlogsPage = () => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [openBlog, setOpenBlog] = useState(false);
+  const [openTag, setOpenTag] = useState(false);
+
+  const { blogs } = useBlogContext();
+
+  const handleOpenBlog = () => setOpenBlog(true);
+  const handleCloseBlog = () => setOpenBlog(false);
+
+  const handleOpenTag = () => setOpenTag(true);
+  const handleCloseTag = () => setOpenTag(false);
 
   return (
     <div>
@@ -13,12 +26,20 @@ export const BlogsPage = () => {
 
       <div id="blogs-container">
         <div id="create-blog">
-          <Button style={{ width: "120px" }} onClick={handleOpen}>
+          <Button style={{ width: "120px" }} onClick={handleOpenTag}>
+            Create Tag
+          </Button>
+          <Button style={{ width: "120px" }} onClick={handleOpenBlog}>
             Create Blog
           </Button>
         </div>
+
+        {blogs.map((blog, index) => (
+          <div key={index}>{blog.title}</div>
+        ))}
       </div>
-      <CreateBlogModal open={open} handleClose={handleClose} />
+      <CreateBlogModal open={openBlog} handleClose={handleCloseBlog} />
+      <CreateTagModal open={openTag} handleClose={handleCloseTag} />
     </div>
   );
 };
